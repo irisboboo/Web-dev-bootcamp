466ceb52b72f4a24b310f591620261293a72a58d
Your Name <your-bhavyasiri.battula26@gmail.com>
2026-08-13 16:25:58 +0530

    add

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,25 @@
 //alert("welcome to web bootcamp!");
+var user=[
+    {
+        "name": "john doe",
+        "gender": "male",
+        "image": "john.png"
+    },
+    {
+         "name": "jane doe",
+        "gender": "female",
+        "image": "jane.png"
+    }
+]
+var curId = 0;
+
+function toggleUser(){
+    curId = (curId + 1) % 2;
+    var userName = document.getElementById("user-name");    
+    var userGender = document.getElementById("user-gender");
+    var userImage = document.getElementById("user-image");
+
+    userName.innerHTML = user[curId].name;
+    userGender.innerHTML = user[curId].gender;
+    userImage.src = user[curId].image;
+}
