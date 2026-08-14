11f08d7df0a6458e0809c47309807059f147f494
Your Name <your-bhavyasiri.battula26@gmail.com>
2026-08-14 07:33:41 +0530

    ch

--- frontend/index.js
+++ frontend/index.js
@@ -9,12 +9,17 @@ var user=[
          "name": "jane doe",
         "gender": "female",
         "image": "jane.png"
-    }
+    },
+    {
+     "name": "summer",
+        "gender": "season",
+        "image": "img.jpeg"
+    } 
 ]
 var curId = 0;
 
 function toggleUser(){
-    curId = (curId + 1) % 2;
+    curId = (curId + 1) % user.length;
     var userName = document.getElementById("user-name");    
     var userGender = document.getElementById("user-gender");
     var userImage = document.getElementById("user-image");
